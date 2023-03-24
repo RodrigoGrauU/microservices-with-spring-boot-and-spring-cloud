db471a3c11143f17a4c37d91b599bc676a3a6716
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2023-03-24 12:27:35 -0300

    15. Using Spring ResponseEntity to Manipulate the HTTP Response

--- src/main/java/com/rodrigo/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/rodrigo/springbootrestapi/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.rodrigo.springbootrestapi.controller;
 import com.oracle.svm.core.annotate.Delete;
 import com.rodrigo.springbootrestapi.bean.Student;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,57 +13,59 @@ import java.util.List;
 public class StudentController {
 
     @GetMapping("student")
-    public Student getStudent() {
-        return new Student(
+    public ResponseEntity<Student> getStudent() {
+        Student student = new Student(
                 1,
                 "Nome",
                 "Sobrenome"
         );
+        return ResponseEntity.ok(student);
     }
 
     @GetMapping("students")
-    public List<Student> getStudents() {
+    public ResponseEntity<List<Student>> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "João", "Silva"));
         students.add(new Student(2, "Silva", "João"));
         students.add(new Student(2, "Marcos", "Santos"));
         students.add(new Student(2, "Carla", "Silva"));
-        return students;
+        return ResponseEntity.ok(students);
     }
 
     @GetMapping("student/{id}/{first-name}/{last-name}")
-    public Student studentPathVariable(@PathVariable("id") int studentId,
+    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId,
                                           @PathVariable("first-name") String firstName,
                                           @PathVariable("last-name") String lastName) {
-        return new Student(studentId, firstName, lastName);
+        Student student = new Student(studentId, firstName, lastName);
+        return ResponseEntity.ok(student);
     }
 
     @GetMapping("student/query")
-    public Student studentRequestVariable(@RequestParam int id,
-                                          @RequestParam String firstName,
-                                          @RequestParam String lastName) {
-        return new Student(id, firstName, lastName);
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
+                                                          @RequestParam String firstName,
+                                                          @RequestParam String lastName) {
+        Student student = new Student(id, firstName, lastName);
+        return ResponseEntity.ok(student);
     }
 
     @PostMapping("students/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student) {
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     @PutMapping("students/{id}/update")
-    public Student updateStudent(@RequestBody Student student, @PathVariable("id") int id) {
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int id) {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
-        return student;
+        return ResponseEntity.ok(student);
     }
 
     @DeleteMapping("student/{id}/delete")
-    public String deleteStudent(@PathVariable("id") int studentId) {
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
         System.out.println(studentId);
-        return "Student deleted successfuly!";
+        return ResponseEntity.ok("Student deleted successfuly!");
     }
 }
