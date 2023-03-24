96dd2d8ca973b98d535c1cfac3aa724b03d93314
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2023-03-24 12:38:57 -0300

    16. Define Base URL for REST API’s in Spring MVC Controller - @RequestMapping

--- src/main/java/com/rodrigo/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/rodrigo/springbootrestapi/controller/StudentController.java
@@ -1,6 +1,5 @@
 package com.rodrigo.springbootrestapi.controller;
 
-import com.oracle.svm.core.annotate.Delete;
 import com.rodrigo.springbootrestapi.bean.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("students")
 public class StudentController {
 
     @GetMapping("student")
@@ -22,17 +22,17 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
-    @GetMapping("students")
+    @GetMapping
     public ResponseEntity<List<Student>> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "João", "Silva"));
         students.add(new Student(2, "Silva", "João"));
-        students.add(new Student(2, "Marcos", "Santos"));
-        students.add(new Student(2, "Carla", "Silva"));
+        students.add(new Student(3, "Marcos", "Santos"));
+        students.add(new Student(4, "Carla", "Silva"));
         return ResponseEntity.ok(students);
     }
 
-    @GetMapping("student/{id}/{first-name}/{last-name}")
+    @GetMapping("{id}/{first-name}/{last-name}")
     public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId,
                                           @PathVariable("first-name") String firstName,
                                           @PathVariable("last-name") String lastName) {
@@ -40,7 +40,7 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
-    @GetMapping("student/query")
+    @GetMapping("query")
     public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
                                                           @RequestParam String firstName,
                                                           @RequestParam String lastName) {
@@ -48,7 +48,7 @@ public class StudentController {
         return ResponseEntity.ok(student);
     }
 
-    @PostMapping("students/create")
+    @PostMapping("create")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
         System.out.println(student.getId());
         System.out.println(student.getFirstName());
@@ -56,14 +56,14 @@ public class StudentController {
         return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
-    @PutMapping("students/{id}/update")
+    @PutMapping("{id}/update")
     public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int id) {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
         return ResponseEntity.ok(student);
     }
 
-    @DeleteMapping("student/{id}/delete")
+    @DeleteMapping("{id}/delete")
     public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId) {
         System.out.println(studentId);
         return ResponseEntity.ok("Student deleted successfuly!");
