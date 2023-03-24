3bb3d1060bc90700ce737c0665c685545aab00a2
Rodrigo Vasconcelos <rodrigoassis1997@gmail.com>
2023-03-24 12:12:39 -0300

    14. Spring Boot DELETE REST API - @DeleteMapping

--- src/main/java/com/rodrigo/springbootrestapi/controller/StudentController.java
+++ src/main/java/com/rodrigo/springbootrestapi/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.rodrigo.springbootrestapi.controller;
 
+import com.oracle.svm.core.annotate.Delete;
 import com.rodrigo.springbootrestapi.bean.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -57,5 +58,11 @@ public class StudentController {
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
         return student;
+    }
+
+    @DeleteMapping("student/{id}/delete")
+    public String deleteStudent(@PathVariable("id") int studentId) {
+        System.out.println(studentId);
+        return "Student deleted successfuly!";
     }
 }
